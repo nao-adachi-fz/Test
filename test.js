d293b5e90d08b7598ebc89ed33029ea292efd67e
Nao Adachi <nao.adachi.fz@hitachi.com>
2017-07-24 09:45:28 +0900

    Commit

---
+++ test.js
@@ -0,0 +1,22 @@
+    console.log('here');
+    var action;
+    $('#test').bind('click', function(e){
+        var now       = new Date().getTime();
+        var lastTouch = $(this).data('lastTouch') || now + 1;  /* In the first time, this will make delta a negative number */
+        var delta     = now - lastTouch;
+        clearTimeout(action);
+
+        e.preventDefault();
+        
+        if(delta < 300 && delta > 0) {
+        	$('#test-out').text('double click');
+        } else {
+            $(this).data('lastTouch', now);
+            action = setTimeout(function(e) {
+            	$('#test-out').text('single click');
+                clearTimeout(action);
+            }, 300);
+        }
+
+        $(this).data('lastTouch', now);
+    });
