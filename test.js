bd3645049c29602167c204916c2e0e1c58d59d8f
nao-adachi-fz <nao.adachi.fz@hitachi.com>
2017-07-24 10:29:46 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
     var action;
 //     $('#test').click(function(){
    $('#test').bind('click', function(e){
+//       e.preventDefault();
+        
         var now       = new Date().getTime();
         var lastTouch = $(this).data('lastTouch') || now + 1;  /* In the first time, this will make delta a negative number */
         var delta     = now - lastTouch;
         clearTimeout(action);
 
-       e.preventDefault();
-        
         if(delta < 300 && delta > 0) {
         	$('#test-out').text('double click');
         } else {
