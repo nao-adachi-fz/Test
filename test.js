885c2a1f931798b666e9b8b7976ca38d4a364cfb
Nao Adachi <nao.adachi.fz@hitachi.com>
2017-08-28 09:29:10 +0900

    Commit

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-$(window).bind('click touchstart', function(e) {
+//$(window).bind('click touchstart', function(e) {
+$(window).click(function(e) {
 	var tagName    = e.target.tagName;
 	
 	$('p.out').text(tagName);
-}
+});
